9a6a028dc6450580ae778e719836370f208ba047
root <root@stacey-airshipdev.lan>
2020-06-09 23:26:05 +0000

    Adding some sushy changes with some un-needed Ansible

--- sushy/entrypoint.sh
+++ sushy/entrypoint.sh
@@ -3,5 +3,13 @@ set -ex
 
 ANSIBLE_USER=${ansible_user:-ubuntu}
 ANSIBLE_SSH_PASS=${ansible_ssh_pass}
+: "${action:="start"}"
 
-sushy-emulator --port 8003 --libvirt-uri "qemu:///system" -i 0.0.0.0
+sushy-emulator --port 8000 --libvirt-uri "qemu:///system" -i 0.0.0.0
+
+if [[ "${action}" == "start" ]]; then
+    ansible-playbook -v -i /opt/ansible/playbooks/inventory.yaml /opt/ansible/playbooks/start.yaml
+else
+    echo "\${action} value ${action} does not match an expected value"
+    exit 1
+fi
