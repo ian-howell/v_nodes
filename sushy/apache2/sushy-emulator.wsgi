124762819e17f12754d1507f67c8fc151db8212c
root <root@stacey-airshipdev.lan>
2020-06-17 15:05:23 +0000

    Adding apache

---
+++ sushy/apache2/sushy-emulator.wsgi
@@ -0,0 +1,1 @@
+from sushy_tools.emulator.main import app as application
--- sushy/entrypoint.sh
+++ sushy/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -ex
 
-ANSIBLE_USER=${ansible_user:-ubuntu}
-ANSIBLE_SSH_PASS=${ansible_ssh_pass}
-: "${action:="start"}"
+a2enmod headers ssl
+a2enconf ssl-params
 
-sushy-emulator --port 8000 --libvirt-uri "qemu:///system" -i 0.0.0.0
+groupadd libvirt
+useradd -g libvirt -M -d /var/lib/libvirt -c "apache sushy user" sushy 
+mkdir -p /var/www/sushy-emulator
 
-if [[ "${action}" == "start" ]]; then
-    ansible-playbook -v -i /opt/ansible/playbooks/inventory.yaml /opt/ansible/playbooks/start.yaml
-else
-    echo "\${action} value ${action} does not match an expected value"
-    exit 1
-fi
+openssl req -newkey rsa:2048 -nodes -keyout /etc/ssl/private/airship_gate_redfish_auth.key -x509 -days 365 -out /etc/ssl/certs/airship_gate_redfish_auth.pem -config <( cat /opt/conf/csr_details.txt ) -extensions 'req_ext'
+
+htpasswd -bc /etc/apache2/sites-available/airship_gate_redfish_auth.htpasswd username password
+
+mkdir -p /home/sushy
+chown -R sushy /home/sushy
+
+source /etc/apache2/envvars
+tail -f /dev/null
+apache2 -DFOREGROUND
