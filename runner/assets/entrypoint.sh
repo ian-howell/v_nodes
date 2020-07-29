8a41de0e47079bcd54b2600294fe7f555b96fc0c
Ubuntu <ubuntu@stacey-k8s-1.lan>
2020-07-29 21:29:18 +0000

    Adding manifests creation

--- runner/assets/entrypoint.sh
+++ runner/assets/entrypoint.sh
@@ -3,10 +3,14 @@ set -ex
 docker info
 curl --insecure https://username:password@127.0.0.1:8443/redfish/v1/Systems/
 
-git clone https://github.com/airshipit/airshipctl.git /opt/airshipctl
+git clone https://github.com/slfletch/airshipctl.git /opt/airshipctl
+git clone https://github.com/slfletch/v_nodes.git /tmp/v_nodes
 
 cd /opt/airshipctl
 git checkout c64ad2db4f3793e0af96fef670752a9aeb45bc90
+
+tail -f /dev/null
+ansible-playbook -v -i /tmp/v_nodes/opt/ansible/playbooks/inventory.yaml /tmp/v_nodes/virsh/assets/opt/ansible/playbooks/create-manifests.yaml -vv
 
 git status
 make docker-image
@@ -21,5 +25,7 @@ export USER=root
 echo "Set disable_coredump false" >> /etc/sudo.conf
 tail -f /dev/null
 ./tools/deployment/24_build_ephemeral_iso.sh
+
+
 #./tools/deployment/25_deploy_ephemeral_node.sh
 tail -f /dev/null
